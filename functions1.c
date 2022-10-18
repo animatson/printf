e5f9ac3d98caf9b96ec83315f2366669b8faaa5c
OSTAR1 <okpochiemeriechi@gmail.com>
2022-10-18 14:35:17 -0700

    functions1.c

--- functions1.c
+++ functions1.c
@@ -2,7 +2,7 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * print_unsigned - Prints an unsigned number
+ * print_unsigned - unsigned number to be print
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
